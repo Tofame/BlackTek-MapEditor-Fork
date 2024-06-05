94fe92ae93465d038693e7a4b6701fb8fae59b2d
codinablack <codinablack@gmail.com>
2024-06-05 04:24:04 -0500

    Drop unused map format and CMake. Replace CMake with Premake.

---
+++ premake5.lua
@@ -0,0 +1,59 @@
+workspace "Black-Tek-Mapeditor"
+   configurations { "Debug", "Release"}
+   platforms { "64" }
+   location ""
+   editorintegration "On"
+
+   project        "Black-Tek-Mapeditor"
+      kind        "ConsoleApp"
+      language    "C++"
+      cppdialect  "C++20"
+      targetdir   "%{wks.location}"
+      objdir      "build/%{cfg.buildcfg}/obj"
+      location    ""
+      files { "source/**.cpp", "source/**.h" }
+      flags {"LinkTimeOptimization", "MultiProcessorCompile"}
+      vectorextensions "AVX"
+      enableunitybuild "On"
+
+      filter "configurations:Debug"
+         defines { "DEBUG" }
+         symbols "On"
+         optimize "Debug"
+      filter {}
+
+      filter "configurations:Release"
+         defines { "NDEBUG" }
+         symbols "On"
+         optimize "Speed"
+      filter {}
+
+      filter "platforms:64"
+         architecture "amd64"
+      filter {}
+
+      filter "system:not windows"
+         buildoptions { "-Wall", "-Wextra", "-pedantic", "-pipe", "-fvisibility=hidden", "-Wno-unused-local-typedefs" }
+      filter {}
+
+      filter "system:windows"
+         openmp "On"
+         characterset "MBCS"
+         debugformat "c7"
+         linkoptions {"/IGNORE:4099"}
+      filter {}
+
+      filter "toolset:gcc"
+         buildoptions { "-fno-strict-aliasing" }
+         buildoptions {"-std=c++20"}
+      filter {}
+
+      filter "toolset:clang"
+         buildoptions { "-Wimplicit-fallthrough", "-Wmove" }
+      filter {}
+
+      filter { "system:macosx", "action:gmake" }
+         buildoptions { "-fvisibility=hidden" }   
+      filter {}
+
+      intrinsics   "On"
