1c9d212c42328da7ca3e6e398ac85dc1a09d777f
Tofame <80583148+Tofame@users.noreply.github.com>
2025-09-13 02:00:49 +0200

    Move load signatures to beginning

--- source/gui.cpp
+++ source/gui.cpp
@@ -359,7 +359,26 @@ bool GUI::LoadDataFiles(wxString& error, wxArrayString& warnings)
 	}
 
 	g_gui.CreateLoadBar("Loading asset files");
-	g_gui.SetLoadDone(0, "Loading metadata file...");
+
+	// Load signatures.toml
+	g_gui.SetLoadDone(0, "Loading signatures.toml file...");
+	std::string exeDir = wxFileName(wxStandardPaths::Get().GetExecutablePath()).GetPath().ToStdString();
+	if(!g_gui.gfx.loadSignatures(exeDir + "\\signatures.toml", error)) {
+		error = "Couldn't load signatures.toml: " + error;
+		g_gui.DestroyLoadBar();
+		UnloadVersion();
+		return false;
+	} /* // Debug tests if signatures.toml work properly
+	else {
+		error = wxString::Format(
+			"[Test 1077, 1098] %d %d",
+			g_gui.gfx.getProtocolVersionByDatSignature(14558),
+			g_gui.gfx.getProtocolVersionByDatSignature(17059)
+		);
+		return false;
+	} */
+
+	g_gui.SetLoadDone(5, "Loading metadata file...");
 
 	wxFileName metadata_path = g_gui.gfx.getMetadataFileName();
 	if(!g_gui.gfx.loadSpriteMetadata(metadata_path, error, warnings)) {
@@ -378,24 +397,6 @@ bool GUI::LoadDataFiles(wxString& error, wxArrayString& warnings)
 		UnloadVersion();
 		return false;
 	}
-
-	// Load signatures.toml
-	g_gui.SetLoadDone(15, "Loading signatures.toml file...");
-	std::string exeDir = wxFileName(wxStandardPaths::Get().GetExecutablePath()).GetPath().ToStdString();
-	if(!g_gui.gfx.loadSignatures(exeDir + "\\signatures.toml", error)) {
-		error = "Couldn't load signatures.toml: " + error;
-		g_gui.DestroyLoadBar();
-		UnloadVersion();
-		return false;
-	} /* // Debug tests if signatures.toml work properly
-	else {
-		error = wxString::Format(
-			"[Test 1077, 1098] %d %d",
-			g_gui.gfx.getProtocolVersionByDatSignature(14558),
-			g_gui.gfx.getProtocolVersionByDatSignature(17059)
-		);
-		return false;
-	} */
 
 	g_gui.SetLoadDone(20, "Loading items.otb file...");
 	if(!g_items.loadFromOtb(wxString(data_path.GetPath(wxPATH_GET_VOLUME | wxPATH_GET_SEPARATOR) + "items.otb"), error, warnings)) {
